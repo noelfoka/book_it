10d1a7a33852df6426051e95a4d665014a4fcfad
noelfoka <nomgnenoel@gmail.com>
2025-01-03 12:34:35 +0100

    add title

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,9 @@ export default function Home() {
     <div className="relative">
       <section className="w-full">
         <div className="relative hero-content text-center mx-4 lg:mx-[15%] py-20">
-          <div className="max-w-lg"></div>
+          <div className="max-w-lg">
+            <h1><span>Book<span>it</span></span></h1>
+          </div>
         </div>
       </section>
     </div>
