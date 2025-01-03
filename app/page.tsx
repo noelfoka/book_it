7436c19b79cd6133728cac500547838bef6f2bfc
noelfoka <nomgnenoel@gmail.com>
2025-01-03 12:32:26 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="relative">
-      <section className="w-full"></section>
+      <section className="w-full">
+        <div className="relative hero-content text-center mx-4 lg:mx-[15%] py-20">
+          <div className="max-w-lg"></div>
+        </div>
+      </section>
     </div>
   );
 }
