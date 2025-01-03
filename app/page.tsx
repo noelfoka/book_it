c0f200585a47cb5df6b34c869ba6e66dae53f064
noelfoka <nomgnenoel@gmail.com>
2025-01-03 12:48:08 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         <div className="relative hero-content text-center mx-4 lg:mx-[15%] py-20">
           <div className="max-w-lg">
             <h1>
-              <div className="bg-secondary">
+              <div className="bg-secondary p-1 mr-1 rounded-md text-white">
               <CalendarCheck className="w-8 h-8 md:w-16 md:h-16" />
               </div>
               <span>
