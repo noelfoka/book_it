102b6371ff620a0bca20fc35183e3b52ae6dce57
noelfoka <nomgnenoel@gmail.com>
2025-01-03 12:42:04 +0100

    add icon

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { CalendarCheck } from "lucide-react";
 import Image from "next/image";
 export default function Home() {
   return (
@@ -6,6 +7,9 @@ export default function Home() {
         <div className="relative hero-content text-center mx-4 lg:mx-[15%] py-20">
           <div className="max-w-lg">
             <h1>
+              <div>
+              <CalendarCheck />
+              </div>
               <span>
                 Book<span>It</span>
               </span>
