47a5c8d90897c15627ffd9d8ce73d3527c11b9e5
noelfoka <nomgnenoel@gmail.com>
2025-01-03 13:04:26 +0100

    add step daisyui

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { CalendarCheck } from "lucide-react";
-import Image from "next/image";
+/*import Image from "next/image";*/
 export default function Home() {
   return (
     <div className="relative">
@@ -8,14 +8,26 @@ export default function Home() {
           <div className="max-w-lg">
             <h1 className="text-3xl md:text-7xl flex justify-center items-center">
               <div className="bg-secondary p-1 mr-1 rounded-md text-white">
-              <CalendarCheck className="w-8 h-8 md:w-16 md:h-16" />
+                <CalendarCheck className="w-8 h-8 md:w-16 md:h-16" />
               </div>
               <span>
                 Book<span>It</span>
               </span>
             </h1>
 
-            <h2 className="py-4 md:py-6 text-xl md:text-4xl font-semibold">Gerez la réservation de vos <span>salles de réunion</span> simplement</h2>
+            <h2 className="py-4 md:py-6 text-xl md:text-4xl font-semibold">
+              Gerez la réservation de vos <span>salles de réunion</span>{" "}
+              simplement
+            </h2>
+
+            <ul className="steps">
+              <li className="step step-info">Fly to moon</li>
+              <li className="step step-info">Shrink the moon</li>
+              <li className="step step-info">Grab the moon</li>
+              <li className="step step-error" data-content="?">
+                Sit on toilet
+              </li>
+            </ul>
           </div>
         </div>
       </section>
