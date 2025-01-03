0250a717978d3d40e8af9e1d658bf82b0b0a33e1
noelfoka <nomgnenoel@gmail.com>
2025-01-03 12:45:46 +0100

    start style icon

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
         <div className="relative hero-content text-center mx-4 lg:mx-[15%] py-20">
           <div className="max-w-lg">
             <h1>
-              <div>
-              <CalendarCheck />
+              <div className="bg-secondary">
+              <CalendarCheck className="w-8 h-8 md:w-16 md:h-16" />
               </div>
               <span>
                 Book<span>It</span>
