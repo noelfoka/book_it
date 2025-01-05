1629dc7bf2ea26c15a8fc6d562ad2c44558f8931
noelfoka <nomgnenoel@gmail.com>
2025-01-05 07:54:08 +0100

    update

---
+++ prisma/migrations/20250105064931_added_company/migration.sql
@@ -0,0 +1,34 @@
+/*
+  Warnings:
+
+  - You are about to drop the `user` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "user";
+PRAGMA foreign_keys=on;
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "famillyName" TEXT,
+    "givenName" TEXT,
+    "email" TEXT NOT NULL,
+    "CompanyId" TEXT,
+    CONSTRAINT "User_CompanyId_fkey" FOREIGN KEY ("CompanyId") REFERENCES "Company" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Company" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "createdById" TEXT,
+    CONSTRAINT "Company_createdById_fkey" FOREIGN KEY ("createdById") REFERENCES "User" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Company_name_key" ON "Company"("name");
