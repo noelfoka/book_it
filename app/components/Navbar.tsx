bfd451e91bbb668e6f20a615b08994451474af91
noelfoka <nomgnenoel@gmail.com>
2025-01-05 08:55:53 +0100

    start navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs';
+import { usePathname } from 'next/navigation';
+import React, { useState } from 'react'
 
 const Navbar = () => {
+
+  const { user } = useKindeBrowserClient();
+  const pathname = usePathname();
+
+  const [loading, setLoading] = useState<boolean>(true);
+
   return (
     <div>Navbar</div>
   )
