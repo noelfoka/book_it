0728f0f2e71f014ae4175ef9419b873f0d05413a
noelfoka <nomgnenoel@gmail.com>
2025-01-05 06:45:21 +0100

    simple post api

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,3 +1,12 @@
 // api permettant d'enregistrer un utilisateur dans la base de données
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
+
+// route pour la création d'un utilisateur
+export async function POST(request: Request) {
+  try {
+    
+  } catch (error) {
+    
+  }
+}
