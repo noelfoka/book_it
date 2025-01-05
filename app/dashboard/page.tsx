68b60dd8e3f8d047d44189800c9144e144c0a0d1
noelfoka <nomgnenoel@gmail.com>
2025-01-05 11:44:22 +0100

    fetch data

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 import Wrapper from "../components/wrapper";
 
@@ -13,13 +13,34 @@ const page = () => {
   const fetchCompanyId = async () => {
     if (user) {
       try {
-        
+        const response = await fetch("/api/users", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            email: user.email,
+            famillyName: user.family_name,
+            givenName: user.given_name
+          })
+        });
+
+        const data = await response.json();
+        setCompanyId(data.companyId || null);
+        setLoading(false);
+
       } catch (error) {
         console.error("Erreur lors de la récupération de l'id de l'entreprise", error);
         setCompanyId(null);
       }
     }
   };
+
+  useEffect(() => {
+    const initializeData = async () => {
+      await fetchCompanyId();
+    }
+  }, [user]);
 
   return (
     <Wrapper>
