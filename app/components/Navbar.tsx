6968273a5a8c899251b5638a00e2e97af87e9c7f
noelfoka <nomgnenoel@gmail.com>
2025-01-05 08:59:44 +0100

    implement loading in useEffect

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs';
 import { usePathname } from 'next/navigation';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Navbar = () => {
 
@@ -8,6 +8,14 @@ const Navbar = () => {
   const pathname = usePathname();
 
   const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    if(user) {
+      setLoading(false);
+    } else {
+      setLoading(true);
+    }
+  })
 
   return (
     <div>Navbar</div>
