d8b4a9793880ce8fb83f57b2a929b92e2ba0cf6e
noelfoka <nomgnenoel@gmail.com>
2025-01-05 07:05:21 +0100

    create a user

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -5,10 +5,39 @@ import prisma from "@/lib/prisma";
 // route pour la création d'un utilisateur
 export async function POST(request: Request) {
   try {
-    const {email, famillyName, givenName} = await request.json();
+    const { email, famillyName, givenName } = await request.json();
 
+    // vérification des champs requis
+    if (!email || !famillyName || !givenName) {
+      return NextResponse.json(
+        { error: "Missing required fields" },
+        { status: 400 }
+      );
+    }
+
+    //vérificatio si l'utilisateur existe déjà dans la base de données
+    let user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      // création d'un utilisateur
+      user = await prisma.user.create({
+        data: {
+          email,
+          famillyName,
+          givenName,
+        },
+      });
+    } else {
+    }
   } catch (error) {
-    console.error('Erreur lors de la création d\'un utilisateur', error);
-    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+    console.error("Erreur lors de la création d'un utilisateur", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
   }
 }
