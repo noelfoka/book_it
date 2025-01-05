281e7aad1c92b7e4259561d99bca5711f7142025
noelfoka <nomgnenoel@gmail.com>
2025-01-05 09:21:47 +0100

    add style to log

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,37 +1,38 @@
-import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs';
-import { CalendarCheck } from 'lucide-react';
-import { usePathname } from 'next/navigation';
-import React, { useEffect, useState } from 'react'
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import { CalendarCheck } from "lucide-react";
+import { usePathname } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
 const Navbar = () => {
-
   const { user } = useKindeBrowserClient();
   const pathname = usePathname();
 
   const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    if(user) {
+    if (user) {
       setLoading(false);
     } else {
       setLoading(true);
     }
-  })
+  });
 
   return (
     <nav>
       <div>
         <div>
-          <h1>
-            <div>
-            <CalendarCheck />
+          <h1 className="text-2xl font-bold flex items-center">
+            <div className="bg-secondary p-1 mr-1 rounded-md text-white">
+              <CalendarCheck />
             </div>
-            <span>Book<span>It</span></span>
+            <span>
+              Book<span className="text-secondary">It</span>
+            </span>
           </h1>
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
