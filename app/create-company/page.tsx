6a57a3c36da97073567228ea4ef77685af97e337
noelfoka <nomgnenoel@gmail.com>
2025-01-05 16:18:46 +0100

    update

--- app/create-company/page.tsx
+++ app/create-company/page.tsx
@@ -9,17 +9,50 @@ const page = () => {
   const [companyName, setCompanyName] = useState<string>("");
   // const [loading, setLoading] = useState<boolean>(true);
 
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    
+    e.preventDefault();
+
+    if (!companyName) {
+      return;
+    }
+
+    try {
+      const response = await fetch("/api/companies", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          email: user?.email,
+          companyName: companyName
+        })
+      });
+
+      if (response.ok) {
+        const message = await response.json();
+        console.log(message);
+        return;
+      }
+
+    } catch (error) {
+      console.error("Erreur lors de la création d'une entreprise", error);
+    }
+
+  };
+
   return (
     <Wrapper>
       <div>
         <h1 className="text-2xl mb-4">Créez une entreprise</h1>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="flex flex-row">
             <input
               type="text"
               id="companyName"
               value={companyName}
+              onChange={(e) => setCompanyName(e.target.value)}
               placeholder="Nom de l'entreprise"
               required
               className="input input-bordered w-full max-w-xs"
