c1a7eb05b6b88889035f3904033d2b7c377c4929
noelfoka <nomgnenoel@gmail.com>
2025-01-05 16:05:10 +0100

    add state hook

--- app/create-company/page.tsx
+++ app/create-company/page.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, { useState } from "react";
 import Wrapper from "../components/wrapper";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 
 const page = () => {
+
+  const { user } = useKindeBrowserClient();
+  const [companyName, setCompanyName] = useState<string>("");
+  // const [loading, setLoading] = useState<boolean>(true);
+
   return (
     <Wrapper>
       <div>
@@ -12,10 +19,14 @@ const page = () => {
             <input
               type="text"
               id="companyName"
+              value={companyName}
               placeholder="Nom de l'entreprise"
               required
               className="input input-bordered w-full max-w-xs"
             />
+
+            <button type="submit" className="btn btn-secondary ml-2">Créez l&apos;entreprise</button>
+
           </div>
         </form>
       </div>
