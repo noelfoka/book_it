3b1516024508144ac539d98295b7bb98512dfa7c
noelfoka <nomgnenoel@gmail.com>
2025-01-06 18:49:46 +0100

    add notification

--- app/employees/[companyId]/page.tsx
+++ app/employees/[companyId]/page.tsx
@@ -1,20 +1,33 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 "use client";
 
-import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs'
-import React, { useState } from 'react'
+import Wrapper from "../../components/Wrapper";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import React, { useState } from "react";
+import Notification from "../../components/Notification";
 
-const page = ({params} : {params: {companyId: string}}) => {
-
+const page = ({ params }: { params: { companyId: string } }) => {
   const { user } = useKindeBrowserClient();
 
-  const [employeeEmail, setEmployeeEmail] = useState<string>('');
-  const [companyName, setCompanyName] = useState<string>('');
+  const [employeeEmail, setEmployeeEmail] = useState<string>("");
+  const [companyName, setCompanyName] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(true);
 
-  return (
-    <div>{params.companyId}</div>
-  )
-}
+  const [notification, setNotification] = useState<string>("");
+  const colseNotification = () => {
+    setNotification("");
+  };
 
-export default page
+  return (
+    <Wrapper>
+      {notification && (
+        <Notification
+          message={notification}
+          onclose={colseNotification}
+        ></Notification>
+      )}
+    </Wrapper>
+  );
+};
+
+export default page;
