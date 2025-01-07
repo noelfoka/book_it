f6c0a3836b88bed751d046733163513aacee2878
noelfoka <nomgnenoel@gmail.com>
2025-01-07 03:18:21 +0100

    route pour la création d'un employé

--- app/api/employees/route.ts
+++ app/api/employees/route.ts
@@ -1,3 +1,16 @@
 // Api pour récupérer les employés d'une entreprise
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
+
+// route pour la création d'un employé
+export async function GET(request: Request) {
+  try {
+    
+  } catch (error) {
+    console.error("Error getting companies", error);
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
